9148a4fdbd267eccbcb329803622d44e6f1b3edf
zznichka <nikasky03@gmail.com>
2020-12-05 12:52:56 +0300

    lesson1

---
+++ task1.py
@@ -0,0 +1,9 @@
+name = input("What is your name:  ")
+print("Hello", name)
+age = input("How old are you? ")
+print( age, "WOW!Me too")
+location = input('Where are you from(country)?  ')
+print(location, 'is a beautiful country:)')
+how_long = input('how many years have you lived there?')
+print('Not bad!')
+
